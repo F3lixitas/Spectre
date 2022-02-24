b960317c5aa0f9269b2d38a887d5f09dc14ca86b
Felix Riedel <69387907+F3lixitas@users.noreply.github.com>
2022-02-24 13:32:19 +0100

    window integration testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+
+#define VK_USE_PLATFORM_XLIB_KHR
 #include <vulkan/vulkan.hpp>
 
+#ifdef __linux
+#include <X11/Xlib.h>
+#endif
+
 int main() {
+    Display* display = XOpenDisplay(nullptr);
+    Screen* screen = DefaultScreenOfDisplay(display);
+    int screenID = DefaultScreen(display);
+    Window window = XCreateSimpleWindow(display, RootWindowOfScreen(screen), 0, 0, 720, 480, 0, BlackPixel(display, screenID),
+                                        WhitePixel(display, screenID));
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    XMapWindow(display, window);
+
     VkApplicationInfo appInfo;
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pNext = nullptr;
@@ -13,14 +27,20 @@ int main() {
 
     std::vector<const char*> layers = {"VK_LAYER_KHRONOS_validation"};
 
+#ifdef __linux
+    const char* extensions[] = {"VK_KHR_xlib_surface", "VK_KHR_surface"};
+#elif _WIN32
+    const char* extensions[] = {"VK_KHR_win32_surface", "VK_KHR_surface"};
+#endif
+
     VkInstanceCreateInfo instCreateInfo;
     instCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instCreateInfo.pNext = nullptr;
     instCreateInfo.flags = 0;
-    instCreateInfo.enabledExtensionCount = 0;
+    instCreateInfo.enabledExtensionCount = 2;
     instCreateInfo.enabledLayerCount = layers.size();
     instCreateInfo.pApplicationInfo = &appInfo;
-    instCreateInfo.ppEnabledExtensionNames = nullptr;
+    instCreateInfo.ppEnabledExtensionNames = extensions;
     instCreateInfo.ppEnabledLayerNames = layers.data();
 
     VkInstance instance;
@@ -37,10 +57,35 @@ int main() {
         VkPhysicalDeviceProperties props;
         vkGetPhysicalDeviceProperties(physicalDevices[i], &props);
         std::cout << "api version : " << props.apiVersion << "\ndriver version : " << props.driverVersion
-        << "\nvendor ID : " << props.vendorID << "\ndevice ID : " << props.deviceID << "\ndevice type : "
-        << props.deviceType << "\ndevice name : " << props.deviceName << "\npipeline cache UUID : "
-        << props.pipelineCacheUUID << "\n\n";
+                  << "\nvendor ID : " << props.vendorID << "\ndevice ID : " << props.deviceID << "\ndevice type : "
+                  << props.deviceType << "\ndevice name : " << props.deviceName << "\npipeline cache UUID : "
+                  << props.pipelineCacheUUID << "\n\n";
     }
+
+    VkSurfaceKHR  surface;
+
+#ifdef __linux
+    VkXlibSurfaceCreateInfoKHR xlibCreateInfo;
+    xlibCreateInfo.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
+    xlibCreateInfo.pNext = nullptr;
+    xlibCreateInfo.flags = 0;
+    xlibCreateInfo.dpy = display;
+    xlibCreateInfo.window = window;
+
+    vkCreateXlibSurfaceKHR(instance, &xlibCreateInfo, nullptr, &surface);
+
+    XEvent e;
+    while(true){
+        XNextEvent(display, &e);
+        if (e.type == Expose) {
+            XFillRectangle(display, window, DefaultGC(display, screenID), 20, 20, 10, 10);
+            XDrawString(display, window, DefaultGC(display, screenID), 10, 50, "hello world", 11);
+        }
+        if (e.type == KeyPress)
+            break;
+    }
+    XCloseDisplay(display);
+#endif
 
     return 0;
 }
