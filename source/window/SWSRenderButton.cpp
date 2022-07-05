308db829705be8476faa8884a8350e16e7e7eb21
Felix Riedel <69387907+F3lixitas@users.noreply.github.com>
2022-07-05 18:22:19 +0200

    divide button into subsections

--- source/window/SWSRenderButton.cpp
+++ source/window/SWSRenderButton.cpp
@@ -2,7 +2,11 @@
 #include <vector>
 #include <iostream>
 
+#define SPECTRE_NEW_WAY
+
 void SWSRenderButton::create(SWSWidgetInfo &info, SWSRenderWindow *parent){
+#ifdef SPECTRE_OLD_WAY
+    /// old way, one single square
     float relPosX = ((float)info.offsetX / parent->getSize().x) * 2 - 1;
     float relPosY = ((float)info.offsetY / parent->getSize().y) * 2 - 1;
     float relSizeX = ((float)info.sizeX / parent->getSize().x) * 2;
@@ -19,4 +23,159 @@ void SWSRenderButton::create(SWSWidgetInfo &info, SWSRenderWindow *parent){
 
     std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
     parent->getRenderer()->addMeshData(vertices, indices);
+#else
+    /// separate by border sections
+    /// 5 pixels for border
+
+    // center section
+    {
+        float relPosX = ((float) (info.offsetX + 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = ((float) (info.sizeX - 10) / parent->getSize().x) * 2;
+        float relSizeY = ((float) (info.sizeY - 10) / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+
+    // left
+    {
+        float relPosX = ((float) info.offsetX / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = ((float) (info.sizeY - 10) / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // right
+    {
+        float relPosX = ((float) (info.offsetX + info.sizeX - 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = ((float) (info.sizeY - 10) / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // top
+    {
+        float relPosX = ((float) (info.offsetX + 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY) / parent->getSize().y) * 2 - 1;
+        float relSizeX = ((float) (info.sizeX - 10) / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // bottom
+    {
+        float relPosX = ((float) (info.offsetX + 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + info.sizeY - 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = ((float) (info.sizeX - 10) / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+
+    // top-left corner
+    {
+        float relPosX = ((float) (info.offsetX) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // top-right corner
+    {
+        float relPosX = ((float) (info.offsetX + info.sizeX - 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // bottom-left corner
+    {
+        float relPosX = ((float) (info.offsetX) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + info.sizeY - 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+    // top-left corner
+    {
+        float relPosX = ((float) (info.offsetX + info.sizeX - 5) / parent->getSize().x) * 2 - 1;
+        float relPosY = ((float) (info.offsetY + info.sizeY - 5) / parent->getSize().y) * 2 - 1;
+        float relSizeX = (5.0f / parent->getSize().x) * 2;
+        float relSizeY = (5.0f / parent->getSize().y) * 2;
+
+        std::vector<SVVertex3D> vertices = {
+                {{relPosX,            relPosY,            0}, {1, 1, 1}, {0, 0}},
+                {{relPosX,            relPosY + relSizeY, 0}, {1, 1, 1}, {0, 1}},
+                {{relPosX + relSizeX, relPosY,            0}, {1, 1, 1}, {1, 0}},
+                {{relPosX + relSizeX, relPosY + relSizeY, 0}, {1, 1, 1}, {1, 1}}
+        };
+        std::vector<uint32_t> indices = {0, 1, 2, 1, 2, 3};
+        parent->getRenderer()->addMeshData(vertices, indices);
+    }
+
+#endif
+
+
+
 }
